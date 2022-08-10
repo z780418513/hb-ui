f1ef8470a40c2ff174f1a18f7c790b7e510c9ac8
zhaochengshui <zhaochengshui@hmtim.com>
2022-08-11 00:19:28 +0800

    日常提交

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 // 导入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+// router
 import router from './router'
+// 清除浏览器默认样式
+import '@/assets/style/reset.css'
 
 
 const app = createApp(App)
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,62 @@
 <template>
   <div>
-    <h1>登入页面</h1>
+    <el-form label-width="120px" class="login-form">
+      <h1 class="form-top">用户登录</h1>
+      <!--用户名-->
+      <el-form-item label="用户名">
+        <el-col :span="18">
+          <el-input v-model="username" placeholder="请输入用户名"></el-input>
+        </el-col>
+      </el-form-item>
+
+      <!--密码-->
+      <el-form-item label="密码">
+        <el-col :span="18">
+          <el-input v-model="password" type="password" placeholder="请输入密码"
+                    show-password></el-input>
+        </el-col>
+
+      </el-form-item>
+
+      <el-button @click="toLogin">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
+import {reactive, toRefs} from "vue";
+
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  setup() {
+    const data = reactive({
+      username: '',
+      password: '',
+    });
+
+    // 登录请求
+    const toLogin= function (){
+
+    }
+
+    return {
+      ...toRefs(data),
+      toLogin
+    };
+  }
 }
 </script>
 
 <style scoped>
+.login-form {
+  border: 2px solid #DCDFE6;
+  text-align: center;
+  border-radius: 8px;
+  margin: 100px auto;
+  width: 500px;
+}
 
+.form-top {
+  margin: 10px auto;
+}
 </style>
